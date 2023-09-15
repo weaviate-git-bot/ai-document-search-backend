10df62f78ba49d5cde88e70bcd84c259027861ca
Petr Janik <peta.janik@email.cz>
2023-09-15 22:01:13 +0200

    Add script to download customer data

---
+++ ai_document_search_backend/utils/download_documents.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from pathlib import Path
+
+import pandas as pd
+import requests
+
+from ai_document_search_backend.utils.relative_path_from_file import relative_path_from_file
+
+SOURCE_DATA_PATH = relative_path_from_file(__file__, "../../data/NTNU_2023v2.xlsx")
+DATA_DOWNLOAD_FOLDER = relative_path_from_file(__file__, "../../data/pdfs")
+
+if os.path.exists(DATA_DOWNLOAD_FOLDER):
+    shutil.rmtree(DATA_DOWNLOAD_FOLDER)
+os.makedirs(DATA_DOWNLOAD_FOLDER)
+
+documents = pd.read_excel(SOURCE_DATA_PATH, engine="openpyxl", sheet_name="Documents")
+real_estate_bonds = pd.read_excel(SOURCE_DATA_PATH, engine="openpyxl", sheet_name="116 Real Estate", usecols=range(19))
+df = documents.merge(real_estate_bonds, on="isin", how="inner")
+# df = df[df["Currency"] == "EUR"]  # try getting only English ones
+df = df[df["MarketNewsTypeName"] == "BondTerms"]
+
+pdf_links = df["link"].tolist()
+print(f"Number of PDFs: {len(pdf_links)}")
+
+for pdf_link in pdf_links:
+    filename = pdf_link.split("/")[-1]
+    print(f"Downloading {filename}...")
+    res = requests.get(pdf_link)
+    with open(Path(DATA_DOWNLOAD_FOLDER) / filename, "wb") as f:
+        f.write(res.content)
