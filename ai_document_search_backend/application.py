58ec5e015e1799b53b87a3e650621fd7331c337e
Petr Janik <peta.janik@email.cz>
2023-11-10 10:35:18 +0100

    Add better logging

--- ai_document_search_backend/application.py
+++ ai_document_search_backend/application.py
@@ -1,3 +1,8 @@
+import logging
+import random
+import string
+import time
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
@@ -11,6 +16,9 @@ from .routers import (
     conversation_router,
 )
 from .services.chatbot_service import ChatbotError
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 def create_app() -> FastAPI:
@@ -37,6 +45,22 @@ def create_app() -> FastAPI:
             status_code=400,
             content={"detail": exc.message},
         )
+
+    @app.middleware("http")
+    async def log_requests(request: Request, call_next):
+        idem = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        logger.info(f"rid={idem} start request path={request.url.path}")
+        start_time = time.time()
+
+        response = await call_next(request)
+
+        process_time = (time.time() - start_time) * 1000
+        formatted_process_time = "{0:.2f}".format(process_time)
+        logger.info(
+            f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}"
+        )
+
+        return response
 
     return app
 
